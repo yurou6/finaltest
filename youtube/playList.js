94148ef821aed4a69be3d53c329fe8b7cc29db43
sofast <shuangrain07@gmail.com>
2021-06-25 22:23:52 +0800

    1

--- youtube/playList.js
+++ youtube/playList.js
@@ -1,16 +1,14 @@
 //YouTube Video ID
 let playList = [
     "y5vLdCh2RJE",
-    "j6GxPQn-3RI",
     "jRg5Ewcnh38",
     "sL3sVUzF9wQ",
-    "mf5tsbcTy4E",
+    "j6GxPQn-3RI"
     ];
     //播放起訖秒數
     let playTime = [
     [36,85],
-    [50,52],
     [122,165],
     [84,117],
-    [146,152]
+    [79,92]
     ];
